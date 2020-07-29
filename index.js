b3c111df1f8ff698dc1f3bcaa5e510b43c405acd
Dennis Murphy <dennis.j.murphy@mac.com>
2020-07-29 13:28:46 +0200

    initial commit

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const compression = require('compression');
+const compression = require("compression");
 
 app.use(compression());
 
-if (process.env.NODE_ENV != 'production') {
+if (process.env.NODE_ENV != "production") {
     app.use(
-        '/bundle.js',
-        require('http-proxy-middleware')({
-            target: 'http://localhost:8081/'
+        "/bundle.js",
+        require("http-proxy-middleware")({
+            target: "http://localhost:8081/",
         })
     );
 } else {
-    app.use('/bundle.js', (req, res) => res.sendFile(`${__dirname}/bundle.js`));
+    app.use("/bundle.js", (req, res) => res.sendFile(`${__dirname}/bundle.js`));
 }
 
-app.get('*', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+// get * must be the last route
+// it ensures that the app will be running regardless of the url
+app.get("*", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(8080, function() {
+app.listen(8080, function () {
     console.log("I'm listening.");
 });
